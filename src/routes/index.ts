e323356a36ce49d2a7b2eb98d53179f99ecce1ce
Lourenço Passos <lo.passos93@gmail.com>
2021-12-08 21:12:28 -0300

    feat: add routes to app

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import { CheckoutBusiness } from "../business/checkout-business";
+import { PromotionBusiness } from "../business/promotions-business";
+import { Controller } from "../controller/controller";
 
 const router = express.Router();
+
+const promotionsBusiness = new PromotionBusiness();
+const checkoutBusiness = new CheckoutBusiness(promotionsBusiness);
+const controller = new Controller(checkoutBusiness);
+
+router.post("/checkout", controller.checkout);
 
 export { router }
